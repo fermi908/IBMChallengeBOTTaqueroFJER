83639a551bbe497f4bd0fc3f25f2653434380c2f
Fernando Josúe Espinoza Ramos <fernando.josue@hotmail.com>
2019-10-03 20:49:07 +0000

    mas puntos y comas

--- app.js
+++ app.js
@@ -31,7 +31,7 @@ app.use(session({
   resave: false,
   saveUninitialized: true,
   cookie: { secure: false }
-}))
+}));
 
 app.get('/chat', function (req, res) {
     res.sendFile(__dirname + '/public/chatv2.html');
@@ -75,7 +75,7 @@ app.get('/initiliaze', function(req, res){
         msgOut += "</section>";
       }
 
-      res.send(msgOut)
+      res.send(msgOut);
   });
 
 });
@@ -106,24 +106,24 @@ app.get('/sendMessage', function (req, res) {
       {
         var additionalText = "<br><iframe " + 
           "src=\"https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3764.180449439749!2d-99.27662588457551!3d19.361337747971394!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x85d200e109a496a5%3A0x29bbf597c634a976!2sLiverpool!5e0!3m2!1sen!2sus!4v1545231476611\"" +
-          "width=\"700\" height=\"400\" frameborder=\"0\""
+          "width=\"700\" height=\"400\" frameborder=\"0\"";
           "style=\"border:0\" allowfullscreen></iframe>";
           delete response.context.BANDERA_UBICACION;
-          callback(res, username, message, response, additionalText)
+          callback(res, username, message, response, additionalText);
       }
       else if(response.context.BANDERA_COTIZAR !== undefined)
       {
          var request = require('request');
          //url to API
-         var tipoPrenda = response.context.CATEGORIA_PRODUCTO
-         var url_database = req.protocol + '://' + req.get('host') + '/calldatabase?tipo=' + tipoPrenda
+         var tipoPrenda = response.context.CATEGORIA_PRODUCTO;
+         var url_database = req.protocol + '://' + req.get('host') + '/calldatabase?tipo=' + tipoPrenda;
           request(url_database, function (error, responseAPI, bodyAPI) {
-            var datos_api = JSON.parse(bodyAPI)
-            index_Start = response.entities[0].location[0]
-            indext_End = response.entities[0].location[1] 
-            response.context.PRENDA = response.input.text.substr(index_Start, indext_End-index_Start)
-            response.context.PROMOCIONES = datos_api.promociones
-            response.context.PROMOCIONES_OK = "true"
+            var datos_api = JSON.parse(bodyAPI);
+            index_Start = response.entities[0].location[0];
+            indext_End = response.entities[0].location[1]; 
+            response.context.PRENDA = response.input.text.substr(index_Start, indext_End-index_Start);
+            response.context.PROMOCIONES = datos_api.promociones;
+            response.context.PROMOCIONES_OK = "true";
 
             assistant.message({
               workspace_id: wconv_workspaceId,
@@ -134,13 +134,13 @@ app.get('/sendMessage', function (req, res) {
                 
                 delete response.context.BANDERA_COTIZAR;
                 session.context = response2.context;
-                callback(res, username, message, response2, additionalText)
+                callback(res, username, message, response2, additionalText);
               });
         });
       }
       else
       {
-        callback(res, username, message, response, additionalText)
+        callback(res, username, message, response, additionalText);
       }
   });
 
@@ -174,7 +174,7 @@ function callback(res, username, message, watsonresponse, additionalText){
       msgOut += "</div>";
       msgOut += "</section>";
     }
-    res.send(msgOut)
+    res.send(msgOut);
 }
 
 // start server on the specified port and binding host
@@ -184,24 +184,24 @@ app.listen(appEnv.port, '0.0.0.0', function() {
 
 app.get("/calldatabase", function(req, res){
   
-  var tipo = req.query.tipo
+  var tipo = req.query.tipo;
   switch(tipo){
     case "Bottom": 
-      promo = "En prendas inferiores tenemos " + Math.floor(Math.random() * Math.floor(25)) + "% de descuento."
+      promo = "En prendas inferiores tenemos " + Math.floor(Math.random() * Math.floor(25)) + "% de descuento.";
       break;
     case "Top": 
-      promo = "En prendas superiores tenemos " + Math.floor(Math.random() * Math.floor(50)) + "% de descuento."
+      promo = "En prendas superiores tenemos " + Math.floor(Math.random() * Math.floor(50)) + "% de descuento.";
       break;
     case "Joyería": 
-      promo = "En joyería tenemos " + (12 + Math.floor(Math.random() * Math.floor(12)))  + " meses sin intereses"
+      promo = "En joyería tenemos " + (12 + Math.floor(Math.random() * Math.floor(12)))  + " meses sin intereses";
       break;
   }
   
   var result = {
     "promociones": promo
-  }
-  res.send(result)
-})
+  };
+  res.send(result);
+});
 
 /*
 function compare(a,b) {
